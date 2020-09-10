aa46f0772c73dbd563d398477998f8e8f5f5a1ab
Michael Gann <michael.gann@live.com>
2020-09-10 12:15:19 -0600

    display players

--- connect-four.js
+++ connect-four.js
@@ -1,23 +1,34 @@
+import { Game } from "./games.js";
 
-window.addEventListener('DOMContentLoaded', e => {
-    let game;
-    const newGameBtn = document.querySelector('button')
-    const player1 = document.getElementById('player-1-name');
-    const player2 = document.getElementById('player-2-name');
-    const formHolder = document.getElementById('form-holder');
-    formHolder.addEventListener('keyup', e => {
-        if (player1.value && player2.value) {
-            newGameBtn.removeAttribute('disabled')
-        } else {
-            newGameBtn.setAttribute('disabled', null)
-        }
-    })
-})
+let game;
 
-class Game {
-    constructor() {
+const updateUI = () => {
+  if (!game) {
+    document.getElementById("board-holder").classList.add("is-invisible");
+  } else {
+    document.getElementById("board-holder").classList.remove("is-invisible");
+    document.getElementById("game-name").innerHTML = game.getName();
+  }
+};
 
+window.addEventListener("DOMContentLoaded", (e) => {
+  const newGameBtn = document.querySelector("button");
+  const player1 = document.getElementById("player-1-name");
+  const player2 = document.getElementById("player-2-name");
+  const formHolder = document.getElementById("form-holder");
+
+  formHolder.addEventListener("keyup", (e) => {
+    if (player1.value && player2.value) {
+      newGameBtn.removeAttribute("disabled");
+    } else {
+      newGameBtn.setAttribute("disabled", null);
     }
-
-
-}
+  });
+  newGameBtn.addEventListener("click", (e) => {
+    game = new Game(player1.value, player2.value);
+    newGameBtn.setAttribute("disabled", true);
+    player1.value = "";
+    player2.value = "";
+    updateUI();
+  });
+});
---
+++ games.js
@@ -0,0 +1,12 @@
+export class Game {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+
+  getName() {
+    return `${this.player1} v.s. ${this.player2}`;
+  }
+}
+
+// export class Game
