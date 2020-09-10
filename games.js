c9443c02fe5e06e5e5cbc8a039d0808e0c786d49
Michael Gann <michael.gann@live.com>
2020-09-10 14:22:42 -0600

    finished indicating current player

--- games.js
+++ games.js
@@ -1,8 +1,15 @@
+import { Column } from "./column.js";
+
 export class Game {
   constructor(player1, player2) {
     this.player1 = player1;
     this.player2 = player2;
     this.currentPlayer = 1;
+    this.columns = [];
+  }
+
+  for (let i = 0; i < 7; i++) {
+    this.columns.push(new Column())
   }
 
   getName() {
@@ -10,9 +17,11 @@ export class Game {
   }
 
   playInColumn() {
+    this.columns[i].add(this.currentPlayer)
+
     if (this.currentPlayer === 1) this.currentPlayer = 2;
     else this.currentPlayer = 1;
+
   }
+
 }
-
-
